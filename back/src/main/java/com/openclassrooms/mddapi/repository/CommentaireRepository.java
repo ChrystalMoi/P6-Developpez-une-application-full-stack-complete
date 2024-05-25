915ca214f49ae9e7b9d50abba3aa5879f363a60a
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-05-25 20:55:33 +0200

    Ajout de méthodes dans les repositories

--- back/src/main/java/com/openclassrooms/mddapi/repository/CommentaireRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/CommentaireRepository.java
@@ -3,5 +3,20 @@ package com.openclassrooms.mddapi.repository;
 import com.openclassrooms.mddapi.entity.Commentaire;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentaireRepository extends JpaRepository<Commentaire, Long> {
+    /**
+     * Trouve les commentaires associés à un article spécifique.
+     * @param articleId l'ID de l'article
+     * @return une liste de commentaires
+     */
+    List<Commentaire> findByArticleId(Long articleId);
+
+    /**
+     * Trouve les commentaires associés à un article spécifique et les trie par date de création décroissante.
+     * @param articleId l'ID de l'article
+     * @return une liste de commentaires triés par date de création décroissante
+     */
+    List<Commentaire> findCommentairesByArticleIdOrderByCreeADesc(Long articleId);
 }
--- back/src/main/java/com/openclassrooms/mddapi/repository/InfoUtilisateurRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/InfoUtilisateurRepository.java
@@ -4,6 +4,9 @@ import com.openclassrooms.mddapi.entity.InfoUtilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InfoUtilisateurRepository extends JpaRepository<InfoUtilisateur,Long> {
+    Optional<InfoUtilisateur> findByEmail(String email);
 }
