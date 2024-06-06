7c76e8f8ef84d57b31059279a4693868cf71d352
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-06-06 21:46:53 +0200

    Création fichiers InfoUtilisateurService, InfoUtilisateurServiceImpl et InfoUtulisateurDetail

---
+++ back/src/main/java/com/openclassrooms/mddapi/service/InfoUtilisateurService.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.mddapi.service;
+
+import com.openclassrooms.mddapi.entity.InfoUtilisateur;
+import com.openclassrooms.mddapi.exception.EmailDejaUtiliseeException;
+import com.openclassrooms.mddapi.exception.EntiteNonTrouveeException;
+import com.openclassrooms.mddapi.exception.MotDePasseInvalideException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface InfoUtilisateurService extends UserDetailsService {
+
+    @Override
+    default UserDetails loadUserByUsername(String nomUtilisateur) throws UsernameNotFoundException {
+        InfoUtilisateur infoUtilisateur = getUserByUsername(nomUtilisateur);
+        if (infoUtilisateur == null) {
+            throw new UsernameNotFoundException("Utilisateur non trouvé avec le nom d'utilisateur: " + nomUtilisateur);
+        }
+        return new InfoUtilisateurDetail(infoUtilisateur);
+    }
+
+    String modifierUtilisateur(InfoUtilisateur userInfo, boolean traiterMotDePasse) throws EmailDejaUtiliseeException, MotDePasseInvalideException;
+
+    String creerUtilisateur(InfoUtilisateur userInfo) throws EmailDejaUtiliseeException, MotDePasseInvalideException;
+
+    InfoUtilisateur getUserByUsername(String username) throws EntiteNonTrouveeException;
+
+    InfoUtilisateur getUserById(Long id) throws EntiteNonTrouveeException;
+}
