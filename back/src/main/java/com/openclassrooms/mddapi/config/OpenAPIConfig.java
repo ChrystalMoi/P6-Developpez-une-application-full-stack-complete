521302283ae8323d1b628c2ea843cb39b0646a70
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-06-28 16:56:05 +0200

    Création du fichier OpenApiConfig

---
+++ back/src/main/java/com/openclassrooms/mddapi/config/OpenAPIConfig.java
@@ -0,0 +1,21 @@
+package com.openclassrooms.mddapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Mon Projet 6", version = "v1")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+
+public class OpenAPIConfig {}
+
