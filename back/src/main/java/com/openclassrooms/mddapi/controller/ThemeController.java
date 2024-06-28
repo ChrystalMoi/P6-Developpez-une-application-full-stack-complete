a348e518e73a916967cec682ca67b60b84cbd0f2
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-06-28 18:10:36 +0200

    Ajout javadocs et docSwagger

--- back/src/main/java/com/openclassrooms/mddapi/controller/ThemeController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/ThemeController.java
@@ -12,7 +12,11 @@ import com.openclassrooms.mddapi.service.InfoUtilisateurServiceImpl;
 import com.openclassrooms.mddapi.service.JwtService;
 import com.openclassrooms.mddapi.service.ThemeService;
 import com.openclassrooms.mddapi.service.ThemeServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
@@ -49,6 +53,21 @@ public class ThemeController {
     /* ================================
         /theme (GET)
     ================================*/
+    /**
+     * Récupère tous les thèmes disponibles dans une liste
+     *
+     * @return Une liste de tous les thèmes disponibles
+     */
+    @Operation(
+            summary = "Récupère tous les thèmes disponibles",
+            description = "Cette méthode permet à un utilisateur connecté de récupérer tous les thèmes disponibles",
+            tags = { "Thèmes" }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Liste des thèmes récupérée avec succès"),
+            @ApiResponse(responseCode = "401", description = "Accès non autorisé"),
+            @ApiResponse(responseCode = "403", description = "Accès refusé")
+    })
     @GetMapping("")
     @Secured("ROLE_USER")
     public List<Theme> getTousLesThemes() {
@@ -58,6 +77,24 @@ public class ThemeController {
     /* ================================
         /theme/{id} (GET)
     ================================*/
+    /**
+     * Récupère un thème par son identifiant
+     *
+     * @param id L'identifiant du thème à récupérer
+     * @return Le thème correspondant à l'identifiant spécifier
+     * @throws EntiteNonTrouveeException Si aucun thème correspondant à l'identifiant n'est trouvé
+     */
+    @Operation(
+            summary = "Récupère un thème par son identifiant",
+            description = "Permet à un utilisateur connecté de récupérer un thème spécifique par son identifiant",
+            tags = { "Thèmes" }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Thème récupéré avec succès"),
+            @ApiResponse(responseCode = "404", description = "Thème non trouvé"),
+            @ApiResponse(responseCode = "401", description = "Accès non autorisé"),
+            @ApiResponse(responseCode = "403", description = "Accès refusé")
+    })
     @GetMapping("/{id}")
     @Secured("ROLE_USER")
     public Theme getThemeParId(@PathVariable("id") final long id) throws EntiteNonTrouveeException {
@@ -67,60 +104,174 @@ public class ThemeController {
     /* ================================
         /theme/{id}/articles (POST)
     ================================*/
+    /**
+     * Crée un nouvel article pour un thème spécifié
+     *
+     * @param id L'identifiant du thème pour lequel l'article est créé
+     * @param jwt Le token JWT contenant les informations d'authentification de l'utilisateur
+     * @param articleDto Les détails de l'article à créer
+     * @return Un message indiquant que l'article a été créé avec succès
+     * @throws EntiteNonTrouveeException Si le thème ou l'utilisateur n'est pas trouvé
+     */
+    @Operation(
+            summary = "Crée un nouvel article pour un thème spécifié",
+            description = "Permet à un utilisateur connecté de créer un nouvel article pour un thème spécifié",
+            tags = { "Articles", "Thèmes" }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Article créé avec succès"),
+            @ApiResponse(responseCode = "404", description = "Thème ou utilisateur non trouvé"),
+            @ApiResponse(responseCode = "401", description = "Accès non autorisé"),
+            @ApiResponse(responseCode = "403", description = "Accès refusé")
+    })
     @PostMapping("/{id}/articles")
     @Secured("ROLE_USER")
-    public Map<String,String> creationArticle(@PathVariable("id") final long id, @RequestHeader(value="Authorization",required=false) String jwt, @RequestBody ArticleDto articleDto) throws EntiteNonTrouveeException {
+    public Map<String, String> creationArticle(
+            @Parameter(description = "L'identifiant du thème pour lequel l'article est créé", required = true)
+            @PathVariable("id") final long id,
+
+            @Parameter(description = "Token JWT pour l'authentification", required = true)
+            @RequestHeader(value = "Authorization", required = false) String jwt,
+
+            @Parameter(description = "Détails de l'article à créer", required = true)
+            @RequestBody ArticleDto articleDto
+    ) throws EntiteNonTrouveeException {
+        // Extraction du nom d'utilisateur à partir du token JWT
         String nomUtilisateur = jwtService.extractNomUtilisateur(jwt.substring(7));
 
+        // Récupération de l'utilisateur à partir du nom d'utilisateur
+        InfoUtilisateur utilisateur = infoUtilisateurService.getUtilisateurParNomUtilisateur(nomUtilisateur);
+
+        // Construction de l'objet Article à partir des données reçues
         Article article = Article.builder()
                 .contenu(articleDto.getContenu())
                 .titre(articleDto.getTitre())
-                .nomUtilisateur(infoUtilisateurService.getUtilisateurParNomUtilisateur(nomUtilisateur))
+                .nomUtilisateur(utilisateur)
                 .theme(themeService.getThemeParId(id))
                 .build();
+
+        // Enregistrement de l'article dans la base de données
         articleService.enregistreArticle(article);
 
-        Map<String,String> map = new HashMap<>();
-        map.put("response","Article créé avec succès");
+        // Création de la réponse avec un message de confirmation
+        Map<String, String> map = new HashMap<>();
+        map.put("response", "Article créé avec succès");
 
         return map;
     }
 
+
     /* ================================
         /theme/{id}/subscription (POST)
     ================================*/
+    /**
+     * Inscrit l'utilisateur connecté à un thème spécifié
+     *
+     * @param id L'identifiant du thème auquel l'utilisateur s'abonne
+     * @param jwt Le token JWT contenant les informations d'authentification de l'utilisateur
+     * @return Un message indiquant le succès de l'inscription au thème ou si le thème est déjà abonné
+     * @throws EntiteNonTrouveeException Si le thème ou l'utilisateur n'est pas trouvé
+     */
+    @Operation(
+            summary = "Inscrit l'utilisateur connecté à un thème spécifié",
+            description = "Permet à un utilisateur connecté de s'abonner à un thème spécifié",
+            tags = { "Abonnements" }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "L'abonnement au thème a été réalisé avec succès"),
+            @ApiResponse(responseCode = "404", description = "Thème ou utilisateur non trouvé"),
+            @ApiResponse(responseCode = "401", description = "Accès non autorisé"),
+            @ApiResponse(responseCode = "403", description = "Accès refusé")
+    })
     @PostMapping("/{id}/subscription")
     @Secured("ROLE_USER")
-    public String inscriptionTheme(@PathVariable("id") final long id, @RequestHeader(value="Authorization",required=false) String jwt) throws EntiteNonTrouveeException {
+    public String inscriptionTheme(
+            @Parameter(description = "L'identifiant du thème auquel l'utilisateur s'abonne", required = true)
+            @PathVariable("id") final long id,
+
+            @Parameter(description = "Token JWT pour l'authentification", required = true)
+            @RequestHeader(value="Authorization", required=false) String jwt
+    ) throws EntiteNonTrouveeException {
+        // Extraction du nom d'utilisateur à partir du token JWT
         String nomUtilisateur = jwtService.extractNomUtilisateur(jwt.substring(7));
+
+        // Récupération de l'utilisateur à partir du service
         InfoUtilisateur utilisateur = infoUtilisateurService.getUtilisateurParNomUtilisateur(nomUtilisateur);
+
+        // Récupération des abonnements actuel de l'utilisateur
         Set<Theme> userSubscriptions = utilisateur.getSubscriptions();
 
-        if (userSubscriptions.add(themeService.getThemeParId(id))) {
-            utilisateur.setSubscriptions(userSubscriptions);
-            infoUtilisateurService.modifierUtilisateur(utilisateur,false);
+        // Récupération du thème à partir de son identifiant
+        Theme theme = themeService.getThemeParId(id);
+
+        // Ajout du thème aux abonnements de l'utilisateur et vérification si l'ajout a été effectué
+        boolean aEteAbonne = userSubscriptions.add(theme);
+
+        // Si l'abonnement a été ajouter avec succès
+        if (aEteAbonne) {
+            // Mise à jour des abonnements de l'utilisateur dans la base de données
+            infoUtilisateurService.modifierUtilisateur(utilisateur, false);
             return "L'abonnement au thème a été réalisé avec succès";
         } else {
+            // Si le thème était déjà abonné
             return "Le thème est déjà abonné";
         }
     }
 
+
     /* ================================
         /theme/{id}/subscription (DELETE)
     ================================*/
+    /**
+     * Endpoint pour désabonner l'utilisateur connecté d'un thème spécifié
+     *
+     * @param id L'identifiant du thème à désabonner
+     * @param jwt Le token JWT contenant les informations d'authentification de l'utilisateur
+     * @return Un message indiquant le résultat de l'opération de désabonnement
+     * @throws EntiteNonTrouveeException Si le thème ou l'utilisateur n'est pas trouvé
+     */
+    @Operation(
+            summary = "Désabonne l'utilisateur d'un thème",
+            description = "Désabonne l'utilisateur connecté d'un thème spécifié par son identifiant",
+            tags = { "Abonnements" }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Désabonnement réussi"),
+            @ApiResponse(responseCode = "404", description = "Thème ou utilisateur non trouvé"),
+            @ApiResponse(responseCode = "401", description = "Accès non autorisé"),
+            @ApiResponse(responseCode = "403", description = "Accès refusé")
+    })
     @DeleteMapping("/{id}/subscription")
     @Secured("ROLE_USER")
-    public String desabonnementTheme(@PathVariable("id") final long id, @RequestHeader(value="Authorization",required=false) String jwt) throws EntiteNonTrouveeException {
+    public String desabonnementTheme(
+            @Parameter(description = "L'identifiant du thème à désabonner", required = true)
+            @PathVariable("id") final long id,
+
+            @Parameter(description = "Token JWT pour l'authentification", required = true)
+            @RequestHeader(value="Authorization", required=false) String jwt
+    ) throws EntiteNonTrouveeException {
+        // Extraction du nom d'utilisateur du token JWT
         String nomUtilisateur = jwtService.extractNomUtilisateur(jwt.substring(7));
+
+        // Récupération de l'utilisateur à partir du service
         InfoUtilisateur utilisateur = infoUtilisateurService.getUtilisateurParNomUtilisateur(nomUtilisateur);
+
+        // Récupération des abonnements actuel de l'utilisateur
         Set<Theme> userSubscriptions = utilisateur.getSubscriptions();
 
+        // Tentative de suppression du thème de la liste des abonnements de l'utilisateur
         if (userSubscriptions.remove(themeService.getThemeParId(id))) {
             utilisateur.setSubscriptions(userSubscriptions);
-            infoUtilisateurService.modifierUtilisateur(utilisateur,false);
-            return "Le désabonnement à été effectué avec succès";
+
+            // Mise à jour de l'utilisateur dans la base de données
+            infoUtilisateurService.modifierUtilisateur(utilisateur, false);
+
+            // Retour d'un message indiquant que le désabonnement a réussi
+            return "Désabonnement réussi";
         } else {
-            return "Le theme n'a pas été enregistré pour l'abonnement";
+            // Retour d'un message indiquant que le thème n'était pas abonné précédemment
+            return "Le thème n'était pas abonné";
         }
     }
+
 }
