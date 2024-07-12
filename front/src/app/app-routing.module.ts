907d0f73b5b7ec4dbe47dcd7da783f0a00601fac
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-07-12 17:21:27 +0200

    Ajout des fichiers de routes et de déclarations + image page erreur

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+/* Gestion des routes (navigation) */
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './features/auth/home/home.component';
+import { MeComponent } from './component/me/me.component';
+import { ConnexionComponent } from './features/auth/connexion/connexion.component';
+import { InscriptionComponent } from './features/auth/inscription/inscription.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 
-// consider a guard combined with canLoad / canActivate route option
-// to manage unauthenticated user to access private routes
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Route pour la page d'accueil - Home
+  { path: 'me', component: MeComponent }, // Route pour le composant Me
+  { path: 'login', component: ConnexionComponent }, // Route pour la page de Connexion
+  { path: 'register', component: InscriptionComponent }, // Route pour la page d'Inscription
+  { path: '**', component: NotFoundComponent } // Route pour la page d'erreur
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
