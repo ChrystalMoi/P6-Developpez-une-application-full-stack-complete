5ec84146eb4a956d14a1b4768400dbe513a60a52
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-07-25 19:01:03 +0200

    Mise à jours de l'interface user

--- front/src/app/interfaces/user.interface.ts
+++ front/src/app/interfaces/user.interface.ts
@@ -1,6 +1,8 @@
+import { Theme } from '../features/articles/interfaces/theme.interface';
 export interface User {
   id: number;
   nom: string;
   email: string;
   roles: string;
+  subscriptions: Theme[];
 }
