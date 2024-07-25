6187a269d2063e3816afa72452bb93f40e87254f
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-07-25 18:59:03 +0200

    Connexion de l'intercepteur

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authentInterceptor } from './interceptors/authent.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync('noop'),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authentInterceptor])),
   ],
 };
