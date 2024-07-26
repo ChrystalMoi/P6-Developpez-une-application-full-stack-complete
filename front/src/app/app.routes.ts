1390a4148fa4db71f5200a4da68a29c1e4aa7529
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-07-26 12:06:06 +0200

    Création/Update de routes article(s)

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -7,21 +7,27 @@ import { InscriptionComponent } from './features/auth/components/inscription/ins
 import { NotFoundComponent } from './component/not-found/not-found.component';
 import { FormulaireComponent } from './features/articles/components/formulaire/formulaire.component';
 import { AuthGuard } from './guards/auth.guard';
+import { ListeArticlesComponent } from './features/articles/components/liste-articles/liste-articles.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Route pour la page d'accueil - Home
 
-  { path: 'me', component: MeComponent, canActivate: [AuthGuard] }, // Route pour le composant Me
+  { path: 'me', component: MeComponent /*canActivate: [AuthGuard]*/ }, // Route pour le composant Me
 
   { path: 'login', component: ConnexionComponent }, // Route pour la page de Connexion
 
   { path: 'register', component: InscriptionComponent }, // Route pour la page d'Inscription
 
   {
-    path: 'article',
+    path: 'creation-article',
     component: FormulaireComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
   }, // Route pour créer un article
+
+  {
+    path: 'articles',
+    component: ListeArticlesComponent /*canActivate: [AuthGuard],*/,
+  }, // Routes pour afficher tous les articles
 
   { path: '**', component: NotFoundComponent }, // Route pour la page d'erreur (TOUJOURS EN DERNIER)
 ];
