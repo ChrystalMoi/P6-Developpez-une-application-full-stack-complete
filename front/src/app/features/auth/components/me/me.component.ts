febe6a0c8cd20c2966875f56745bd546e4961519
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-07-26 10:58:08 +0200

    Changement de structure + création méthode deconnexion + ajustement du scss

--- front/src/app/features/auth/components/me/me.component.ts
+++ front/src/app/features/auth/components/me/me.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../../../component/menu/menu.component';
+import { SessionService } from '../../../../services/session.service';
 
 @Component({
   selector: 'app-me',
@@ -13,11 +14,16 @@ export class MeComponent implements OnInit {
   moreInfoVisible: boolean = false;
   public onError = false;
 
-  constructor() {}
+  constructor(private sessionService: SessionService) {}
 
   ngOnInit(): void {}
 
   showMore() {
     this.moreInfoVisible = !this.moreInfoVisible;
+  }
+
+  deconnexion() {
+    this.sessionService.logOut();
+    console.info('Déconnecté');
   }
 }
