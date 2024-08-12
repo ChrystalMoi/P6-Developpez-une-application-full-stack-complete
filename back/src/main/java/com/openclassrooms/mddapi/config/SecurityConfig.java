9285e3e073e606959072af345f8805a4d6603ba7
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-08-12 19:17:27 +0200

    Retrait des imports inutile

--- back/src/main/java/com/openclassrooms/mddapi/config/SecurityConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/config/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Classe de configuration de la sécurité pour l'application
