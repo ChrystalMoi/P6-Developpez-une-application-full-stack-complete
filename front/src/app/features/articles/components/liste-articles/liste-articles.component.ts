501f44e351e752a1a0b6f98f07f8aba4f26ce5a1
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-08-15 19:15:43 +0200

    Created : Création de liste pour afficher les articles

--- front/src/app/features/articles/components/liste-articles/liste-articles.component.ts
+++ front/src/app/features/articles/components/liste-articles/liste-articles.component.ts
@@ -1,15 +1,20 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, inject, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuComponent } from '../../../../component/menu/menu.component';
+import { CarteArticleComponent } from '../carte-article/carte-article.component';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { ArticlesService } from '../../services/articles.service';
 
 @Component({
   selector: 'app-liste-articles',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, CarteArticleComponent, AsyncPipe, JsonPipe],
   templateUrl: './liste-articles.component.html',
   styleUrl: './liste-articles.component.scss',
 })
 export class ListeArticlesComponent {
+  articles$ = inject(ArticlesService).tousLesArticles();
+
   constructor(private router: Router, private ngZone: NgZone) {}
 
   creationArticle() {
