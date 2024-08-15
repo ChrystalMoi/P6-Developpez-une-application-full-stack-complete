258d324da3aaf397aca88e97016e21a13a07e1a6
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-08-15 19:19:01 +0200

    Initialisation Commentaire Article et Detail Article

---
+++ front/src/app/features/articles/components/commentaire-article/commentaire-article.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commentaire-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './commentaire-article.component.html',
+  styleUrl: './commentaire-article.component.scss'
+})
+export class CommentaireArticleComponent {
+
+}
---
+++ front/src/app/features/articles/components/detail-article/detail-article.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-article.component.html',
+  styleUrl: './detail-article.component.scss',
+})
+export class DetailArticleComponent {}
