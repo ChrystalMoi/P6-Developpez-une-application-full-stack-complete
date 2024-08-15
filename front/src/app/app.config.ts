a037931cec435590d6482bcf5de6d1ab4c6f078f
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-08-15 19:12:44 +0200

    Update : Configuration de la date local française

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  LOCALE_ID,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -9,6 +13,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
     provideAnimationsAsync('noop'),
     provideHttpClient(withInterceptors([authentInterceptor])),
   ],
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import * as fr from '@angular/common/locales/fr';
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(fr.default);
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
