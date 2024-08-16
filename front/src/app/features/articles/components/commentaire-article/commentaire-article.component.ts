6c732345a1a1cdccba3eea7d758470ca00a786fe
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-08-16 20:15:09 +0200

    Created : Début de l'implémentation du composant commentaire utilisateur pour une page détail d'article

--- front/src/app/features/articles/components/commentaire-article/commentaire-article.component.ts
+++ front/src/app/features/articles/components/commentaire-article/commentaire-article.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Commentaire } from '../../interfaces/commentaire.interface';
 
 @Component({
   selector: 'app-commentaire-article',
   standalone: true,
   imports: [],
   templateUrl: './commentaire-article.component.html',
-  styleUrl: './commentaire-article.component.scss'
+  styleUrl: './commentaire-article.component.scss',
 })
 export class CommentaireArticleComponent {
-
+  @Input() commentaire?: Commentaire;
 }
