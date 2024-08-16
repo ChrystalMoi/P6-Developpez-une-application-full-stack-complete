c0469aa6ab3fe2d6a5c2e96d26355938c8cb0c67
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-08-16 20:13:31 +0200

    Update : Rendre une carte article cliquable pour avoir le détail

--- front/src/app/features/articles/components/carte-article/carte-article.component.ts
+++ front/src/app/features/articles/components/carte-article/carte-article.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Article } from '../../interfaces/article.interface';
 import { DatePipe, JsonPipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-carte-article',
   standalone: true,
-  imports: [JsonPipe, DatePipe],
+  imports: [JsonPipe, DatePipe, RouterModule],
   templateUrl: './carte-article.component.html',
   styleUrl: './carte-article.component.scss',
 })
