da5c1c140d5cae7731587932c0707a7874de7933
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-08-18 18:59:10 +0200

    Update : Renommage du fichier + création d'une méthodes pour afficher tous les commentaires

--- front/src/app/features/articles/services/commentaires.service.ts
+++ front/src/app/features/articles/services/commentaires.service.ts
@@ -2,11 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ArticleResponse } from '../interfaces/articleResponse.interface';
+import { Commentaire } from '../interfaces/commentaire.interface';
+import { CommentaireCreateRequest } from '../interfaces/commentaireCreateRequest.interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MessagesService {
+export class CommentaireService {
   // Définition du chemin de base pour la requête à l'API
   private pathService = 'api/article';
 
@@ -15,13 +17,20 @@ export class MessagesService {
 
   // Méthode pour envoyer un commentaire a un article
   public envoi(
-    articleId: number,
-    messageRequest: string
+    articleId: string,
+    messageRequest: CommentaireCreateRequest
   ): Observable<ArticleResponse> {
     // Envoie une requête POST à l'API avec l'identifiant de l'article et le commentaire
     return this.httpClient.post<ArticleResponse>(
       `${this.pathService}/${articleId}/commentaire`,
       messageRequest
     );
+  }
+
+  public tousLesCommentaires(articleId: string): Observable<Commentaire[]> {
+    const comm = this.httpClient.get<Commentaire[]>(
+      `${this.pathService}/${articleId}/commentaire`
+    );
+    return comm;
   }
 }
