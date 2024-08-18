b1e2ea30481b750ae3e4d13f5a87baf549f138e2
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-08-18 19:00:48 +0200

    Update : Mise a jour du template + style

--- front/src/app/features/articles/components/commentaire-article/commentaire-article.component.ts
+++ front/src/app/features/articles/components/commentaire-article/commentaire-article.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Commentaire } from '../../interfaces/commentaire.interface';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-commentaire-article',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './commentaire-article.component.html',
   styleUrl: './commentaire-article.component.scss',
 })
