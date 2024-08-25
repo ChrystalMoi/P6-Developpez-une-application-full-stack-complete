9ce0df7104994a6fbbc74ff4b04ff6ad898a2d6f
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-08-25 18:00:12 +0200

    Update : Application des deux nouveaux layout

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -9,33 +9,49 @@ import { FormulaireComponent } from './features/articles/components/formulaire/f
 import { AuthGuard } from './guards/auth.guard';
 import { ListeArticlesComponent } from './features/articles/components/liste-articles/liste-articles.component';
 import { DetailArticleComponent } from './features/articles/components/detail-article/detail-article.component';
+import { LayoutAuthComponent } from './component/layout-auth/layout-auth.component';
+import { LayoutConnectedComponent } from './component/layout-connected/layout-connected.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Route pour la page d'accueil - Home
 
-  { path: 'me', component: MeComponent, canActivate: [AuthGuard] }, // Route pour le composant Me
-
-  { path: 'login', component: ConnexionComponent }, // Route pour la page de Connexion
-
-  { path: 'register', component: InscriptionComponent }, // Route pour la page d'Inscription
+  {
+    path: '',
+    component: LayoutAuthComponent,
+    children: [
+      { path: 'login', component: ConnexionComponent }, // Route pour la page de Connexion
+      { path: 'register', component: InscriptionComponent }, // Route pour la page d'Inscription
+    ],
+  },
 
   {
-    path: 'creation-article',
-    component: FormulaireComponent,
-    //canActivate: [AuthGuard],
-  }, // Route pour créer un article
+    path: '',
+    component: LayoutConnectedComponent,
+    /*canActivate: [AuthGuard]*/
+    children: [
+      {
+        path: 'me',
+        component: MeComponent,
+        pathMatch: 'full',
+      }, // Route pour le composant Me
+      {
+        path: 'articles',
+        component: ListeArticlesComponent,
+        pathMatch: 'full',
+      }, // Routes pour afficher tous les articles
+      {
+        path: 'creation-article',
+        component: FormulaireComponent,
+        pathMatch: 'full',
+      }, // Route pour créer un article
 
-  {
-    path: 'articles',
-    component: ListeArticlesComponent,
-    //canActivate: [AuthGuard],
-  }, // Routes pour afficher tous les articles
-
-  {
-    path: 'detail-article/:id',
-    component: DetailArticleComponent,
-    /*canActivate: [AuthGuard],*/
-  }, // Routes pour afficher l'articles sélectionner
+      {
+        path: 'detail-article/:id',
+        component: DetailArticleComponent,
+        pathMatch: 'full',
+      }, // Routes pour afficher l'articles sélectionner
+    ],
+  },
 
   { path: '**', component: NotFoundComponent }, // Route pour la page d'erreur (TOUJOURS EN DERNIER)
 ];
